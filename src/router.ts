e81015141ef00ed5d3cf04c0f752a40b869f80db
P-Chan <hello@0x50.io>
2019-12-25 12:13:48 +0900

    feat: CORS をサーバーレスに対応する

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import Router from 'express-promise-router'
 
 import {
@@ -7,6 +8,9 @@ import {
 } from './controllers/index'
 
 const router = Router()
+
+// CORS をサーバーレスに対応する
+router.options('*', cors())
 
 router.get('/accounts', accountsController.index)
 router.get('/assets', assetsController.index)
